15365ea69940f94e1a1eac3dfaf71bbdfaf41798
Yadamzer <yadamzerterefe09@gmail.com>
2024-08-17 03:50:27 -0700

    New tables or Classes added

--- microblog/app/config.py
+++ microblog/app/config.py
@@ -1,4 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KET') or 'you-will-never-guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or\
+    'sqlite:///' + os.path.join(basedir, 'app.db')
