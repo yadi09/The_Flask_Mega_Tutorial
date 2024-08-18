0756d1bf78ddb1d7eb5d98e4929d8943dec256f6
Yadamzer <yadamzerterefe09@gmail.com>
2024-08-18 09:15:56 -0700

    Adding user profile

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,11 +1,16 @@
-from app import db
+from app import db, login
 from datetime import datetime, timezone
 from typing import Optional
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -19,6 +24,10 @@ class User(UserMixin, db.Model):
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -28,6 +37,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 
 class Post(db.Model):
