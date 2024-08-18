c91d65f9fed2250e1a2c2ca591abccc0f5eff6bf
Yadamzer <yadamzerterefe09@gmail.com>
2024-08-18 13:22:35 -0700

    Add pro editer

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -2,8 +2,8 @@ from app import db
 from app.models import User
 import sqlalchemy as sa
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -32,3 +32,8 @@ class RegistrationForm(FlaskForm):
         ))
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,1 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
